b3a2ea4cee2869ff2f9dd36a7a5aa50663a7da37
Yaroslav Semenov <yaryk2013@gmail.com>
2022-01-06 23:22:08 +0200

    comleted task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on(
+  'timeupdate',
+  throttle(() => {
+    player.getCurrentTime().then(function (seconds) {
+      localStorage.setItem(STORAGE_KEY, seconds);
+    });
+  }, 1000),
+);
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
