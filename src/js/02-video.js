d81dd467857c60a4670327da5088011ff781244b
Yaroslav Semenov <yaryk2013@gmail.com>
2022-01-09 21:30:31 +0200

    fix bugs

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,10 +8,24 @@ const STORAGE_KEY = 'videoplayer-current-time';
 player.on(
   'timeupdate',
   throttle(() => {
-    player.getCurrentTime().then(function (seconds) {
-      localStorage.setItem(STORAGE_KEY, seconds);
-    });
+    player
+      .getCurrentTime()
+      .then(function (seconds) {
+        localStorage.setItem(STORAGE_KEY, seconds);
+      })
+      .catch(function (error) {});
   }, 1000),
 );
 
-player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+player
+  .setCurrentTime(localStorage.getItem(STORAGE_KEY))
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
